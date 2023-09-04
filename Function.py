e6ca7cebfd3f073dca2dcb9df5e9cfe8cc354a5e
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-09-04 12:17:23 +0330

    Updating function.py

--- Function.py
+++ Function.py
@@ -42,3 +42,23 @@ def operator(x, y, *, op='+'):
         return x / y 
     
 operator(3, 6, 5, 7, op='-') # by using * we can't give positional argments more than defined ones
+# here, the op is keyword Only argument
+# * is bare variable argument parameter
+
+
+## we have Positional Only argument. everything before / in function arguments are positional only argument.
+
+def operator(x, /, y, *, op='+'):
+    if op == '+' :
+        return x + y
+    elif op == '-':
+        return x - y
+    elif op == '/':
+        return x / y 
+    
+operator(3, 6, op='-') ## we can't give arguments like : operator(x = 5, 6, op = '-')
+
+## Generally programmers define a function like:
+# def func(* , ...) after * put the keywords arguments
+# the reason is because of the changability feature of functions. the place of arguments can be changed and 
+# #then if we didn't use * at starting definition of the func, our previous codes don't work
