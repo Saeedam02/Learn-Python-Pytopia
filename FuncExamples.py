cb7c01909795da698342f658421f3be3bed04198
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-09-05 14:13:05 +0330

    updating function example and adding make sentence function

--- FuncExamples.py
+++ FuncExamples.py
@@ -1,4 +1,4 @@
-# First example: Frequency of unique elements in an iterable
+# First example: get Frequency of unique elements in an iterable
 my_str = 'abdjcnkdakmdnkp'
 
 
@@ -15,7 +15,15 @@ print(char_freq)
 # approach 2
 
 
-def char_counter(s):
+def char_counter(s:str) ->dict:
+    """
+    get Frequency of unique elements in an iterable
+    :param s: INput string
+    :returns: A dictionary of characters frequency.
+    Example:
+    >>>char_count('abc)
+    {'a': 1, 'b': 1, 'c': 1}
+    """
     char_freq = {}
     for char in s:
         char_freq[char] = char_freq.get(char, 0) +1 # 0 in define for char_freq[char] when char isn't in char_freq
@@ -29,3 +37,35 @@ char_counter(my_str)
 
 from collections import Counter
 print(Counter(my_str))
+
+###### Second example: make sentence
+
+# approach 1
+subjects = ['I', 'You']
+verbs = ['Play' , 'Love']
+objects = ['Hochey' , 'Football']
+sentences=[]
+for s in subjects:
+    for v in verbs:
+        for o in objects:
+           sent = f'{s} {v} {o}'
+           sentences.append(sent)
+print(sentences)
+
+#approach 2
+from typing import Iterable
+def make_sentense(subjects: Iterable[str], verbs: Iterable[str], objects:Iterable[str]) ->list:
+    """
+    Generate all possible sentences from inputs
+    :param subjests: Input sentences subjects
+    :param verbs: Input sentences verbs
+    :param objects: Input sentences objects
+    :returns: List of all possible sentences
+    """
+    import itertools # This library is used to write all Permutations of some lists
+
+    sentences_2 = itertools.product(subjects, verbs, objects)
+    sentences_2 = list(map(lambda sentence: ''.join(sentence), sentences))
+    return sentences_2
+
+make_sentense(subjects,verbs,objects)
