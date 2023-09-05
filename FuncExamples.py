68d63c329206c8f2e2790ade617bf6c6a029a7f5
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-09-05 13:48:00 +0330

    updating function example

--- FuncExamples.py
+++ FuncExamples.py
@@ -1,7 +1,8 @@
+# First example: Frequency of unique elements in an iterable
 my_str = 'abdjcnkdakmdnkp'
 
-char_freq = {}
 
+char_freq = {}
 # approach 1 
 for char in my_str:
     if char in char_freq:
@@ -13,9 +14,18 @@ print(char_freq)
 
 # approach 2
 
-char_freq = {}
 
-for char in my_str:
-    char_freq[char] = char_freq.get(char, 0) +1 # 0 in define for char_freq[char] when char isn't in char_freq
+def char_counter(s):
+    char_freq = {}
+    for char in s:
+        char_freq[char] = char_freq.get(char, 0) +1 # 0 in define for char_freq[char] when char isn't in char_freq
 
-print(char_freq)
+    return char_freq
+
+my_str = 'abdjcnkdakmdnkp'
+char_counter(my_str)
+
+# approach 3
+
+from collections import Counter
+print(Counter(my_str))
