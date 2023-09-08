d9220413dde52bdd219bb8f9df9b46223e66f308
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-09-08 09:12:06 +0330

    updating class.py

--- Class.py
+++ Class.py
@@ -2,6 +2,8 @@ class PeopleOfIran:
     #class attributes
     nationality = 'Iran'
     official_language = 'Farsi'
+
+    # dunder method
     def __init__(self, fname, lname, age, location):
         #instance attributes
         self.fn = fname
@@ -9,10 +11,16 @@ class PeopleOfIran:
         self.age = age
         self.loc = location
 
+    # this dunder method is used to print what it returns when we create an object from class and we just print its name
+    def __str__(self):
+        return 'Hi Dear'
+    
     # instance method
     def speak(self):
         print(f'#MahsaAmini')
 
     def description(self):
         print(f' Hi, I am {self.fn} {self.ln} from {self.loc}')
-         
+
+Nika = PeopleOfIran('Nika', 'Shakarami', 16, 'Tehran')
+
