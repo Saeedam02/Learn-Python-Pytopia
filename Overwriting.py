17840c35e28c70aa68459a4d4e72ec09347279cb
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-09-16 15:23:31 +0330

    Addinf __add__ function to the Point class

--- Overwriting.py
+++ Overwriting.py
@@ -3,6 +3,10 @@ t1 = (1, 2, 3)
 t2 = (3, 2, 8)
 list(zip(t1, t2)) #-> [(1,3),(2,2),(3,8)]
 
+# for writing the __add__ function for our calss, we can use the following approach:
+list(map(lambda item: item[0] + item[1], zip(t1, t2)))
+# second approach
+list(map(sum, zip(t1, t2)))
 
 class Point:
     def __init__(self, *args):
@@ -20,6 +24,8 @@ class Point:
         return abs(self) < abs(obj)
     
     def __add__(self, other):
+            args = map(sum, zip(self.vector, other.vector))
+            return Point(*args)
 
        
 # my_points = [Point(2, 4, 3), Point(5, 9, 8), Point(3, 7, -1), Point(8, 6, 8), Point(6, 6, 11), Point(1, 1, -6) ]
@@ -28,3 +34,4 @@ class Point:
 # print(sorted(my_points))
 p1 = Point(1, 2, 4)
 p2 = Point(3, 2, 8)
+print(Point(3, 2, 8)+Point(1, 2, 4))
