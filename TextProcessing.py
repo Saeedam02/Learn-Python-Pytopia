60d2517ace751e078fff096f6f35f7fa79f70a68
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-09-19 22:02:50 +0330

    Updating Text processing with OOP with ABC method

--- TextProcessing.py
+++ TextProcessing.py
@@ -72,3 +72,46 @@ pipe = TextPipline(
 
 text = ' Woman, Life, Freedom(1401)'
 print(pipe.transform(text))
+
+# Better approach for this processing is using ABC library
+from abc import ABC, abstractmethod
+
+class TextProcessor(ABC):
+    @abstractmethod
+    def transform(self, text):
+        pass
+
+class ConvertCase(TextProcessor):
+    def __init__(self, casing='lower'):
+        self.casing = casing
+    
+        
+    def transform(self, text):
+        if self.casing == 'lower':
+            return  text.lower()
+        elif self.casing == 'upper':
+            return text.upper()
+        elif self.casing == 'title':
+            return self.title()
+    
+class RemoveDigit(TextProcessor):
+    def transform(self, text):
+        return ''.join(filter(lambda char: not char.isdigit(), text))
+    
+class RemoveSpace(TextProcessor):
+    def transform(self, text):
+        return ''.join(text.split())
+    
+class RemovePunkt(TextProcessor):
+    def transform(self, text):
+        return ''.join(filter(lambda char: char not in string.punctuation, text))
+    
+class TextPipline(TextProcessor):
+    def __init__(self, *args):
+        self.transformers = args
+
+    def transform(self, text):
+        for tf in self.transformers:
+            text = tf.trannsform(text)
+        return text
+    
