8e475bba94f9e08ac247fc21ed26b97f922f4393
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-01 07:40:29 +0330

    Updating reading file codes

--- ReadFile.py
+++ ReadFile.py
@@ -14,3 +14,15 @@ with open('documents.txt') as reader:
     print(reader.read())
 
 # when we come out of the index of 'with', it closes the file automatically
+# there are two approach for reading a file:
+# LINE BY LINE: 
+for line in reader:
+    print(line , end='')
+
+# especified characthers:
+reader.read(1000)
+
+# Best way:
+with open('documents.txt') as reader:
+    for lline in reader:
+        print(line , end='')
