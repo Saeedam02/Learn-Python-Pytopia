ea04ba2d902ba9a3440e6b20ba9f272736a34878
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-03 11:48:06 +0330

    Updating counting number of words example for reading file

--- ReadfileExamples.py
+++ ReadfileExamples.py
@@ -3,11 +3,23 @@
 import string
 
 with open('documents.txt') as f:
-    count = f.read()
+    content = f.read()
 
 def preprocess_text(text):
     text = ''.join([char if char not in string.punctuation else ' ' for char in text ])
     return text.lower()
 
-count = preprocess_text(count)
-print(count)
+text = preprocess_text(content)
+print(text)
+
+def count_word(text):
+    """
+    Counting the number of each word
+    :param text: Input
+    :returns: Count dictionary which is contains number of each words
+    """
+    count = {}
+    for word in text.split():
+        count[word] = count.get(word,0) + 1
+
+    return count
