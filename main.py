8bf0591b904cb9c220505546c96f63cc54070550
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-06 11:48:42 +0330

    Updating main packaes file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # This file is related to packages in python
+# inside of each package it is possible to have other sub packages like : pack.sub_paCK1.MOD3
+# inside of a package file we can have a __init__ file which will be run when we run the main file.
+# we can use this dunder method to initialize our packages.
 
-import pkg.mod1, pkg.mod2
+import pack # in this way, by default, python doesn't do anything and if we use pack.mod1, we will face with error.
+import pack.mod2
+# for calling an attribute from this mpdule, we follow like:
+pack.mod2.foo()
+
+from pack.mod1 import foo # in this way, we put foo attribute inside of the global namespace.
+# for calling an attribute from this mpdule, we follow like:
+foo()
+print(foo)
+
+from pack.mod1 import foo
+from pack.mod2 import foo #@ foo overwrite on the first foo.
