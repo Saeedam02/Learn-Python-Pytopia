cba1e79e2936888d9ca19c89d830f381983d2c62
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-13 10:04:28 +0330

    Adding decorators with optional arguments

--- decorators/Fancy_Decorator.py
+++ decorators/Fancy_Decorator.py
@@ -18,3 +18,29 @@ def greet(name):
     print(f"Hello {name}")
 
 print(greet('Saeed'))
+
+#Decorators that Can Optionally Take Arguments
+
+def repeat(_func=None, *, num_times=2):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper_repeat(*args, **kwargs):
+            for _ in range(num_times):
+                value = func(*args, **kwargs)
+            return value
+        return wrapper_repeat
+
+    if _func is None:
+        return decorator_repeat
+    else:
+        return decorator_repeat(_func)
+
+# _func=None
+@repeat(num_times=4)
+def say_hi(name):
+    print(f"Hello {name}")
+
+# _func=say_hi
+@repeat
+def say_hi(name):
+    print(f"Hello {name}")
