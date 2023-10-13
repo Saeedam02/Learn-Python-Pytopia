9770d507fa4ef53d9c4c5ac669df6296a4112159
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-13 10:15:12 +0330

    Stateful Decorators

--- decorators/Fancy_Decorator.py
+++ decorators/Fancy_Decorator.py
@@ -44,3 +44,22 @@ def say_hi(name):
 @repeat
 def say_hi(name):
     print(f"Hello {name}")
+
+
+#Stateful Decorators
+
+def count_calls(func):
+    @functools.wraps(func)
+    def wrapper_count_calls(*args, **kwargs):
+        wrapper_count_calls.num_calls += 1
+        print(f"Call {wrapper_count_calls.num_calls} of {func.__name__!r}")
+        return func(*args, **kwargs)
+
+    # set attribute before return
+    wrapper_count_calls.num_calls = 0
+    return wrapper_count_calls
+
+
+@count_calls
+def say_whee():
+    print("Whee!")
