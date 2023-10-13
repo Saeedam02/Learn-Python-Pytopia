aa83ff39006dbbde31d7cd5f3f34e1e9d58b3438
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-13 12:03:49 +0330

    Adding Classes as Decorators for countcalls class

--- decorators/ClassesAsDecorators.py
+++ decorators/ClassesAsDecorators.py
@@ -1,3 +1,4 @@
+# setting attribute for a object of a class
 class Counter:
     def __init__(self, start=0):
         self.count = start
@@ -7,4 +8,26 @@ class Counter:
         print(f"Current count is {self.count}")
 
 counter = Counter()
-counter() # This pbject is callable because we set a attribute for it.
+counter() # This pbject is callable because we set a attribute for it. it's equal with counter.__call__()
+
+# class as a decorator:
+import functools
+
+
+class CountCalls:
+    def __init__(self, func, *, num_times=2):
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.num_calls = 0
+
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print(f"Call {self.num_calls} of {self.func.__name__!r}")
+        return self.func(*args, **kwargs)
+
+
+@CountCalls
+def say_whee():
+    print("Whee!")
+
+say_whee()
