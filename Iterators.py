f02ac94f41b7befbe38878a1aa1e61531f9fd2d0
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-15 08:40:59 +0330

    Updating iterators

--- Iterators.py
+++ Iterators.py
@@ -6,3 +6,19 @@ next(myiter)
 
 # next(obj) is same as obj.__next__()
 myiter.__next__()
+
+# Working of for loop for Iterators
+# create an iterator object from that iterable
+iter_obj = iter(my_list)
+
+# infinite loop
+while True:
+    try:
+        # get the next item
+        element = next(iter_obj)
+        # do something with element
+    except StopIteration:
+        # if StopIteration is raised, break from loop
+        break
+# Note that when you run this while or a for loop on an iterator, after that if you want to go through that,
+# you need to initialize it again.beacuse after loop, the next of the iterator is none and it's false.
