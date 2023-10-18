1d6ad3af060f56ec160ffb8c4f789346f2dbb4ab
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-18 09:55:11 +0330

    Updating Itertools

--- Itertools.py
+++ Itertools.py
@@ -20,5 +20,29 @@ list(better_grouper(nums, 4))
 
 list(better_grouper_two(nums, 4))
 
+############# Combination with itertools
+bills = [20, 20, 20, 10, 10, 10, 10, 10, 5, 5, 1, 1, 1, 1, 1]
+set(it.combinations(bills, 5)) # 5 is length of combinations
+
+list(it.combinations_with_replacement([1, 2], 2))
+
+############# Permutations with itertools
+list(it.permutations(['a', 'b', 'c'])) #جایگشت های ممکن
+
+def evens():
+    """Generate even integers, starting with 0."""
+    n = 0
+    while True:
+        yield n
+        n += 2
+
+def odds():
+    """Generate odd integers, starting with 1."""
+    n = 1
+    while True:
+        yield n
+        n += 2
+
+
 # for more details about itertools:
 # https://docs.python.org/3/library/itertools.html
